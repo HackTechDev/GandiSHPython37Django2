150b299d5b087724c758f310fd2da3834f58e376
samuel.gondouin <samuel.gondouin@gandi.net>
2018-12-31 08:57:32 +0100

    Fix urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
